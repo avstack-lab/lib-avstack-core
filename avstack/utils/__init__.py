828a05c9cf717b8e69f24fa095bcfceb65834606
Spencer H <spencer.hallyburton@duke.edu>
2023-09-20 13:06:22 -0400

    Run formatting

--- avstack/utils/__init__.py
+++ avstack/utils/__init__.py
@@ -6,8 +6,8 @@
 # @Description:
 
 
+import avstack.utils.decorators
+
 from . import pools  # import to apply patch to multiprocess pool
 from .other import IterationMonitor, check_xor_for_none
 from .stats import mean_confidence_interval
-
-import avstack.utils.decorators
--- avstack/utils/decorators.py
+++ avstack/utils/decorators.py
@@ -1,13 +1,14 @@
-import os
 import cProfile
+import os
+
 from .other import IterationMonitor
 
 
-def profileit(name, folder=''):
+def profileit(name, folder=""):
     """A decorator to profile a function
 
     Use it as:
-    
+
     @profileit("profile_for_func1_001")
     def func1(...)
     ...
@@ -30,13 +31,15 @@ def profileit(name, folder=''):
             # Note use of name from outer scope
             prof.dump_stats(name)
             return retval
+
         return wrapper
+
     return inner
 
 
 class FunctionTriggerIterationMonitor:
     """A decorator to monitor iteration timing
-    
+
     Use it as:
 
     @FunctionTriggerIterationMonitor(print_rate=1/2)
@@ -44,14 +47,17 @@ class FunctionTriggerIterationMonitor:
     ...
 
     """
+
     def __init__(self, print_rate) -> None:
         if callable(print_rate):
-            raise TypeError('Did you pass the `print_rate` parameter to the decorator?')
+            raise TypeError("Did you pass the `print_rate` parameter to the decorator?")
         self.timer = IterationMonitor(print_rate=print_rate, print_method="real_time")
 
     def __call__(self, func):
         decorator_self = self  # why need this?
+
         def wrapper(*args, **kwargs):
             decorator_self.timer.tick()
             return func(*args, **kwargs)
+
         return wrapper
