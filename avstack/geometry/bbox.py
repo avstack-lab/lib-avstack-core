f354a0b79f5f73162d0c720d04a44787a833e62c
Spencer Hallyburton <20426598+roshambo919@users.noreply.github.com>
2024-07-25 14:59:41 -0400

    Add box center calculation

--- avstack/geometry/bbox.py
+++ avstack/geometry/bbox.py
@@ -439,8 +439,11 @@ class Box3D:
     def center(self):
         if self.where_is_t == "center":
             return self.t
+        elif self.where_is_t == "bottom":
+            # HACK: assumes in ground plane...
+            return self.t + np.array([0, 0, self.h / 2])
         else:
-            raise NotImplementedError("Need to do this")
+            raise NotImplementedError
 
     @property
     def center_global(self):
