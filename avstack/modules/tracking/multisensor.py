365eff4ae9cf9dff384f73a9492d3d25b2779f3f
spencer@primus <spencer.hallyburton@duke.edu>
2024-07-29 17:57:00 -0400

    Allow for none FOV input to tracker

--- avstack/modules/tracking/multisensor.py
+++ avstack/modules/tracking/multisensor.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 if TYPE_CHECKING:
-    from avstack.geometry import ReferenceFrame, Shape
+    from avstack.geometry import Shape
 
 from avstack.config import MODELS, ConfigDict
 from avstack.datastructs import DataContainer
@@ -39,7 +39,6 @@ class MeasurementBasedMultiTracker(BaseModule):
         self,
         detections: Dict[int, DataContainer],
         fovs: Dict[int, Union["Shape", np.ndarray]],
-        platforms: Dict[int, "ReferenceFrame"],
         check_reference: bool = True,
         *args,
         **kwargs,
@@ -50,55 +49,37 @@ class MeasurementBasedMultiTracker(BaseModule):
         """
 
         for ID in detections:
-            # NOTE: no need to align frames since all inputs are global
-            pts_ref = [trk.position for trk in self.tracker.tracks_active]
-            # align reference frame for checking observables
-            # pts_ref = [
-            #     trk.position.change_reference(platforms[ID], inplace=False)
-            #     if check_reference
-            #     else trk.position
-            #     for trk in self.tracker.tracks_active
-            # ]
-
-            # use the FOV model to filter for observable tracks
             if detections[ID] is None:
                 continue
-            try:
-                # for fov shape that has a check point method
-                trks_observable = []
-                for trk, pos in zip(self.tracker.tracks_active, pts_ref):
-                    if fovs[ID].check_point(pos.x):
-                        trks_observable.append(trk)
-            except AttributeError:
-                # for an array of points that needs a polygon method
-                pos_to_check = [pos.x[:2] for pos in pts_ref]
-                trks_observable = (
-                    []
-                    if len(pos_to_check) == 0
-                    else [
-                        trk
-                        for trk, in_h in zip(
-                            self.tracker.tracks_active,
-                            parallel_in_polygon(pos_to_check, fovs[ID]),
-                        )
-                        if in_h
-                    ]
-                )
+    
+            # NOTE: no need to align frames since all inputs are global
+            pts_ref = [trk.position for trk in self.tracker.tracks_active]
 
-            # if (detections[ID].timestamp == 0.74) and (ID == 2):
-            #     tracks_out_tmp = DataContainer(
-            #         frame=self.tracker.frame,
-            #         timestamp=self.tracker.timestamp,
-            #         data=self.tracker.tracks_confirmed,
-            #         source_identifier=self.name,
-            #     )
-            #     dists = []
-            #     for i, track_i in enumerate(tracks_out_tmp):
-            #         for j, track_j in enumerate(tracks_out_tmp):
-            #             if j <= i:
-            #                 continue
-            #             dists.append(track_i.box.distance(track_j.box))
-            #     import pdb; pdb.set_trace()
+            # use the FOV model to filter for observable tracks
+            if fovs[ID] is None:
+                trks_observable = self.tracker.tracks_active
+            else:
+                try:
+                    # for fov shape that has a check point method
+                    trks_observable = []
+                    for trk, pos in zip(self.tracker.tracks_active, pts_ref):
+                        if fovs[ID].check_point(pos.x):
+                            trks_observable.append(trk)
+                except AttributeError:
+                    # for an array of points that needs a polygon method
+                    pos_to_check = [pos.x[:2] for pos in pts_ref]
+                    trks_observable = (
+                        []
+                        if len(pos_to_check) == 0
+                        else [
+                            trk
+                            for trk, in_h in zip(
+                                self.tracker.tracks_active,
+                                parallel_in_polygon(pos_to_check, fovs[ID]),
+                            )
+                            if in_h
+                        ]
+                    )
 
             # update the tracks with the new detections
             self.tracker(
@@ -122,11 +103,7 @@ class MeasurementBasedMultiTracker(BaseModule):
                 for j, track_j in enumerate(tracks_out_tmp):
                     if j <= i:
                         continue
-                    dists.append(track_i.box.distance(track_j.box))
-            if any([d < 1 for d in dists]):
-                import pdb
-
-                pdb.set_trace()
+                    dists.append(track_i.box.distance(track_j.box, check_reference=check_reference))
 
         # format as data container
         tracks_out = DataContainer(
